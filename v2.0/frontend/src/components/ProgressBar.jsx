4ff2426924a64ee1032c49001b1d3ce85591c775
drkpn5848 <drkpn.in@gmail.com>
2026-05-24 12:53:42 +0530

    -

--- v2.0/frontend/src/components/ProgressBar.jsx
+++
@@ -1,9 +0,0 @@
-const ProgressBar = ({isProgress}) => {
-    return (
-        <>
-            {isProgress && <div className='progress'></div>}
-        </>
-    );
-}
-
-export default ProgressBar;
