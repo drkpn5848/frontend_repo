a1514b0d45b16241cc84e6ff9d4d16e44d6aed78
drkpn5848 <drkpn.in@gmail.com>
2026-05-24 13:57:49 +0530

    -

---
+++ v5.0/backend/gateway/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from controllers.init import *
+
+app = FastAPI()
+
+#Enable Cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:5173"],
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+#Registers all routers
+app.include_router(AuthenticationRouter)
+
+@app.get("/")
+def home():
+    return "Started..."
