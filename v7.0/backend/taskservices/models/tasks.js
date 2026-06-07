de12ed8a9d0ad0b4337b2f296593bb6fb97f6b88
drkpn5848 <drkpn.in@gmail.com>
2026-06-07 11:56:43 +0530

    -

---
+++ v7.0/backend/taskservices/models/tasks.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    assignedto: {type: Number, required: true},
+    priority: {type: Number, required: true},
+    deadline: {type: String, required: true},
+    status: {type: Number, required: true},
+    createdby: {type: Number, required: true}
+},{
+    timestamps: {createdAt: 'createdat', updatedAt: 'updatedat'}
+});
+
+const Tasks = mongoose.model("tasks", taskSchema);
+
+export default Tasks;
